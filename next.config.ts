1b10970d725ddc6e331ab1569f396423aea7e9e9
[elmony] <jimmysalau@gmail.com>
2026-02-14 12:33:14 +0100

    [elmony] Fixing iframe headers - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -17,14 +17,11 @@ const nextConfig: NextConfig = {
       {
         source: "/:path*",
         headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors *",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *" },
         ],
       },
     ];
